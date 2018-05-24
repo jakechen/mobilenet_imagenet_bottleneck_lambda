a96776564838cf0f59ec15086eca210b0b96bba9
Jake Chen <jakejchen@gmail.com>
2018-05-24 14:07:35 -0500

    changed from download_file to get_object

--- mxnet/lambda_function.py
+++ mxnet/lambda_function.py
@@ -19,43 +19,40 @@ def lambda_handler(event, context):
     for record in event['Records']:
         s3_bucket = record['s3']['bucket']['name']
         s3_key = record['s3']['object']['key']
-        logger.debug(s3_bucket)
-        logger.debug(s3_key)
-
-        if s3_key[-4:]=='.jpg':
-            img_path = '/tmp/{}'.format(s3_key) # where to download image
-            feat_path = '/tmp/{}.npy'.format(s3_key) # where to save features
+        logger.debug("S3 bucket: {}".format(s3_bucket))
+        logger.debug("S3 key: {}".format(s3_key))
             
-            # load selected MobileNet model
-            model = vision.get_model(name='mobilenet1.0', pretrained=True, root='/tmp/.mxnet/models')
+        # load selected MobileNet model
+        model = vision.get_model(name='mobilenet1.0', pretrained=True, root='/tmp/.mxnet/models')
+        
+        # download file to memory
+        response = s3.get_object(
+            Bucket=s3_bucket,
+            Key=s3_key
+        )
+        
+        # decode and preprocess image
+        image = mx.image.imdecode(response['Body'].read())
+        image = image.astype('float32')/255
+        image = mx.image.color_normalize(image,
+                                         mean=mx.nd.array([0.485, 0.456, 0.406]),
+                                         std=mx.nd.array([0.229, 0.224, 0.225]))
+        image = image.reshape([1, -1, image.shape[0], image.shape[1]])
+        
+        # get bottleneck features
+        features = model.features(image)
+        
+        # save features to buffer and upload
+        with BytesIO() as b:
+            np.save(b, features.asnumpy())
             
-            # download file to /tmp/[s3_key]
-            s3.download_file(Bucket=s3_bucket,
-                             Key=s3_key,
-                             Filename=img_path)
-            
-            # load and preprocess image
-            with open("./dog.0.jpg", 'rb') as fp:
-                str_image = fp.read()
-            image = mx.image.imdecode(str_image)
-            image = image.astype('float32')/255
-            image = mx.image.color_normalize(image,
-                                             mean=mx.nd.array([0.485, 0.456, 0.406]),
-                                             std=mx.nd.array([0.229, 0.224, 0.225]))
-            image = image.reshape([1, -1, image.shape[0], image.shape[1]])
-            
-            # get bottleneck features
-            features = model.features(image)
-            
-            # save features to /tmp/[s3_key].npy
-            features = features.asnumpy()
-            np.save(open(feat_path, 'wb'), features)
-            
-            # upload features to s3://[s3_bucket]/[s3_key].npy
-            s3.upload_file(Filename=feat_path,
-                          Bucket=s3_bucket,
-                          Key=s3_key+'.npy')
-            
+            b.seek(0)
+            response = s3.put_object(
+                Body = b,
+                Bucket=s3_bucket,
+                Key="{}.npy".format(s3_key)
+            )
+        
             logging.info(response)
 
-            return
+        return
